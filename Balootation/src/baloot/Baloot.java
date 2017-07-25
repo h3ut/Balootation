4f0db6f7bad8276e43bb54fd9d6d31dda63f1761
RedDragon <RedDragon@DESKTOP-8I0CU2A>
2017-07-25 15:04:01 +0300

    added Class CardHolder to handle players cards and move them around easier.

--- Balootation/src/baloot/Baloot.java
+++ Balootation/src/baloot/Baloot.java
@@ -25,34 +25,38 @@ public class Baloot {
         frame.addMouseListener(graf);
         
         CardShuffler.RandomShuffle(graf.getCards());
-        ArrayList<ArrayList<Card>> dealed = CardDealer.dealCardsNormal(graf.getCards(), 3);
-        CardDealer.dealRestOfCardsNormal(graf.getCards(), dealed, 3);
+        CardHolder cardHolder = CardDealer.dealCardsNormal(graf.getCards(), 3);
+        CardDealer.dealRestOfCardsNormal(cardHolder, graf.getCards(), 3);
+        
+//        ArrayList<ArrayList<Card>> dealed = CardDealer.dealCardsNormal(graf.getCards(), 3);
+//        CardDealer.dealRestOfCardsNormal(graf.getCards(), dealed, 3);
         
         
-        
-        graf.setPlayerMe(dealed.get(0));
-        graf.setPlayerThemFirst(dealed.get(1));
-        graf.setPlayerFriend(dealed.get(2));
-        graf.setPlayerThemSecond(dealed.get(3));
+        graf.setAllPlayers(cardHolder);
+//        graf.setPlayerMe(dealed.get(0));
+//        graf.setPlayerThemFirst(dealed.get(1));
+//        graf.setPlayerFriend(dealed.get(2));
+//        graf.setPlayerThemSecond(dealed.get(3));
         
         
         
         while(true){
             System.out.println(graf.getUserInput());
             CardShuffler.threeAces(graf.getCards());
-            dealed = CardDealer.dealCardsNormal(graf.getCards(), 3);
-            CardDealer.dealRestOfCardsNormal(graf.getCards(), dealed, 3);
+            cardHolder = CardDealer.dealCardsNormal(graf.getCards(), 3);
+            CardDealer.dealRestOfCardsNormal(cardHolder, graf.getCards(), 3);
         
-            Collections.sort(dealed.get(0));
-            Collections.sort(dealed.get(1));
-            Collections.sort(dealed.get(2));
-            Collections.sort(dealed.get(3));
+            cardHolder.sort();
+//            Collections.sort(dealed.get(0));
+//            Collections.sort(dealed.get(1));
+//            Collections.sort(dealed.get(2));
+//            Collections.sort(dealed.get(3));
             
-            
-            graf.setPlayerMe(dealed.get(0));
-            graf.setPlayerThemFirst(dealed.get(1));
-            graf.setPlayerFriend(dealed.get(2));
-            graf.setPlayerThemSecond(dealed.get(3));
+            graf.setAllPlayers(cardHolder);
+//            graf.setPlayerMe(dealed.get(0));
+//            graf.setPlayerThemFirst(dealed.get(1));
+//            graf.setPlayerFriend(dealed.get(2));
+//            graf.setPlayerThemSecond(dealed.get(3));
         }
         
     }
